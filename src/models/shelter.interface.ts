17c0e0c64ecaf594e1a966ad4bd36a219ecea7dc
David Shaw <davidshaw337@gmail.com>
2021-07-09 09:55:27 -0500

    add House class

--- src/models/shelter.interface.ts
+++ src/models/shelter.interface.ts
@@ -6,6 +6,15 @@ interface Shelter {
     isWaterproof: boolean
 }
 
+class House implements Shelter {
+    name: string
+    address: string
+    hasBathroom: boolean
+    canAccomodateGuests: boolean
+    isWaterproof: boolean
+    floors: number
+    cost: number
+}
 /**
  * TODO: Create At least Two type of shelters extend the Shelter interface
  */

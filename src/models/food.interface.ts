e0908ff8024e3ba3619911127bff3e8f3d66ddde
Jad Ismail <jad.ismail@norhart.com>
2021-07-09 17:52:13 +0300

    Create color enums

--- src/models/food.interface.ts
+++ src/models/food.interface.ts
@@ -1,10 +1,22 @@
 interface Food {
     name: string
-    color: string // TODO: Create enum of the basic colors
+    color: Color 
     size: string // TODO: Create enum for size
     calories: number
     vitamins: string // TODO: 1- Create enum with 3 vitamins list | 2- Make this field an array of vitamins
 }
+
+enum Color {
+    ORANGE,
+    BLUE,
+    BLACK,
+    RED,
+    WHITE,
+    PINK,
+    PURPLE
+}
+
+
 
 /**
  * TODO: Create Two food classes for example: Vegetable and Fruits that extend Food interface

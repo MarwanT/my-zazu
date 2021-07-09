1cebce57e717effdf380b269df3a9b6db8d56256
Marwan Toutounji <marwan.toutounji@gmail.com>
2021-07-09 15:53:48 +0300

    Add a zazu interface

---
+++ src/models/zazu.interface.ts
@@ -0,0 +1,11 @@
+interface Zazu {
+    name: string
+    lastName: string
+    dob: Date
+    canSwim: boolean
+    canFly: boolean
+    canWalk: boolean
+    type: string // TODO: Create Type enum listing two options at least: Animal or Plant
+}
+
+// TODO: Create Two type of Zazus for example: Cat, Dog, Animal

cc014e3d3e48cb15441b018c9f416155c19b08b4
Marwan Toutounji <marwan.toutounji@gmail.com>
2021-07-09 15:53:17 +0300

    Add a food interface

---
+++ src/models/food.interface.ts
@@ -0,0 +1,11 @@
+interface Food {
+    name: string
+    color: string // TODO: Create enum of the basic colors
+    size: string // TODO: Create enum for size
+    calories: number
+    vitamins: string // TODO: 1- Create enum with 3 vitamins list | 2- Make this field an array of vitamins
+}
+
+/**
+ * TODO: Create Two food classes for example: Vegetable and Fruits that extend Food interface
+ */

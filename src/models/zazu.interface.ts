142075d915c64ad8e4e97ddc9e23d0be0eb6b1ae
Fabio Orlinski <fabio.orlinski@norhart.com>
2021-07-09 12:00:46 -0300

    Add dog class

--- src/models/zazu.interface.ts
+++ src/models/zazu.interface.ts
@@ -5,7 +5,7 @@ interface Zazu {
     canSwim: boolean
     canFly: boolean
     canWalk: boolean
-    type: ZazuType // TODO: Create Type enum listing two options at least: Animal or Plant
+    type: ZazuType
 }
 
 enum ZazuType {
@@ -13,6 +13,24 @@ enum ZazuType {
     Manager = "plant",
 }
 
+class Dog implements Zazu {
+    name: string
+    lastName: string
+    dob: Date
+    canSwim: boolean
+    canFly: boolean
+    canWalk: boolean
+    type: ZazuType
 
+    public constructor(name: string = "dogName", lastName: string = "dogLastName", dob: Date = new Date()) {
+        this.name = name;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.canSwim = true;
+        this.canFly = false;
+        this.canWalk = true;
+        this.type = ZazuType.Animal
+    }
+}
 
 // TODO: Create Two type of Zazus for example: Cat, Dog, Animal

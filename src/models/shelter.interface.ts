3eccdc2066b8699aaedc9a26d5a3da41d16ce4c1
Marwan Toutounji <marwan.toutounji@gmail.com>
2021-07-09 15:53:26 +0300

    Add a shelter interface

---
+++ src/models/shelter.interface.ts
@@ -0,0 +1,11 @@
+interface Shelter {
+    name: string
+    address: string
+    hasBathroom: boolean
+    canAccomodateGuests: boolean
+    isWaterproof: boolean
+}
+
+/**
+ * TODO: Create At least Two type of shelters extend the Shelter interface
+ */
